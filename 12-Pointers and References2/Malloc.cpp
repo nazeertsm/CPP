5adc73e4eba02b2ccba58a204c6079392f477aa4
nazeertsm <nazeer.mif@gmail.com>
2022-07-16 17:02:01 +0530

    malloc

---
+++ 12-Pointers and References2/Malloc.cpp
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include <stdio.h>
+using namespace  std;
+
+
+int main(){
+
+    int *P1;
+    int *P2;
+    /*Now this malloc ( ) function returns int pointer.
+    So we have to type-caste it and say it is integer pointer.
+    Now this line will allocate heap memory.
+    Remember, whenever you say malloc then only it will allocate in heap. P1 will get the address
+    */
+    P1=  (int *)malloc(5*sizeof(int)); //malloc() function allocate 5*4kb=20kb memory //C lang
+
+    printf("%d\n", P1);
+    P2 = new int[5];  //c++
+    printf("%d\n", P2);
+
+    return 0;
+}
