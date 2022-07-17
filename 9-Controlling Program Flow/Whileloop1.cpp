061addc09eb10c4cf20abaa4780f3218587b3ade
nazeertsm <nazeer.mif@gmail.com>
2022-07-17 06:12:07 +0530

    while loop

---
+++ 9-Controlling Program Flow/Whileloop1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int i = 1;
+
+    while (i <= 5)
+    {
+
+        // printf("%s\n", "MySirG");
+        printf("\nMySirG");
+
+        i++;
+    }
+}
---
+++ 9-Controlling Program Flow/Whileloop2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int i = 1, n;
+
+    printf("Enter a number");
+    scanf("%d", &n);
+
+    while (i <= n)
+    {
+
+        // printf("%s\n", "MySirG");
+        printf("\nMySirG");
+
+        i += 1;
+    }
+}
