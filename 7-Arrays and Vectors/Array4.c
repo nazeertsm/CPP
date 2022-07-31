e56c8ebd693fe7931faa7fd28c13c59ec9550c30
nazeertsm <nazeer.mif@gmail.com>
2022-07-31 16:04:31 +0530

    array file added:

---
+++ 7-Arrays and Vectors/Array4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num[10], i, se = 0, so = 0;
+    printf("Enter 10 numbers");
+
+    for (int i = 0; i < 9; i++)
+
+        scanf("%d", &num[i]);
+
+    for (i = 0; i < 9; i++)
+    {
+        if (num[i] % 2 == 0)
+
+            se = se + num[i];
+        else
+            so = so + num[i];
+    }
+
+    
+
+    printf("\n sum of all even numbers: %d", se);
+    printf("\n sum of all odd numbers: %d", so);
+
+    printf("\n");
+
+    return 0;
+}
