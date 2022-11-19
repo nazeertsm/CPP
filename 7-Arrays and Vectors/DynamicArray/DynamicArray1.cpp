30cb07e70d059dc038bf7123cccc61717e16f178
ahmedna <nazeer.ahmed@navis.com>
2022-11-19 20:03:15 +0530

    vector

--- 7-Arrays and Vectors/DynamicArray/DynamicArray1.cpp
+++ 7-Arrays and Vectors/DynamicArray/DynamicArray1.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 int main(){
 
-    int *p = new int[20];
+    int *ptr1 = NULL; //Pointer initialized with NULL
+    ptr1 = new int;
+
+    int *ptr2 = new int; //combine declaration aof pointer and their assignment--single 4 bytes memory created
+
+    //pointer-variable = new data-type[size];
+    int *p = new int[20]; // Memory created in Heap, Base address assigned to p pointer
     *p=120;
     cout<<"the value of p is : "<<*p<<endl;
     cout<<"the address of p is : "<<p<<endl;
--- 7-Arrays and Vectors/DynamicArray/DynamicArray2.cpp
+++ 7-Arrays and Vectors/DynamicArray/DynamicArray2.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main(){
 
-    
+
+int *p1  = new int();
+cout<<*p1<<endl;  // zero or garbage value, since the value not assighned during initialization
+
+//pointer-variable = new data-type(value);
+int *p2  = new int(25); 
+cout<<*p2<<endl;  //25
+
+
 }
